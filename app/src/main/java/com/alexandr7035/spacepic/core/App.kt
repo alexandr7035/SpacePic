80f2e5987f0ba6866aac257a7607de03e5c72264
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-11-26 10:03:32 +0300

    Create application class

---
+++ app/src/main/java/com/alexandr7035/spacepic/core/App.kt
@@ -0,0 +1,18 @@
+package com.alexandr7035.spacepic.core
+
+import android.app.Application
+import com.alexandr7035.spacepic.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Setup timber
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
