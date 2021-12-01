ee0996c09399d26aa8462658a767ce8d90f99fb9
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-02 01:23:53 +0300

    Make navigation bar transparent

--- app/src/main/java/com/alexandr7035/spacepic/MainActivity.kt
+++ app/src/main/java/com/alexandr7035/spacepic/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.alexandr7035.spacepic
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.navigationBarColor = Color.TRANSPARENT
+        window.statusBarColor = Color.TRANSPARENT
     }
 }
