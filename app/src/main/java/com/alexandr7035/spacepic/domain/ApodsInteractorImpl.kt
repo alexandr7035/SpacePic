3bcc1b2b3d0a33304b7c6c50254129b5ff2aa3c1
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-03 16:53:28 +0300

    Remove old code from interactor

--- app/src/main/java/com/alexandr7035/spacepic/domain/ApodsInteractorImpl.kt
+++ app/src/main/java/com/alexandr7035/spacepic/domain/ApodsInteractorImpl.kt
@@ -15,22 +15,6 @@ class ApodsInteractorImpl(
 ): ApodsInteractor {
     override suspend fun fetchApods(lastApodDate: Long): ApodsDomain {
 
-        // FIXME move logics to a separate file
-        // Page size
-        val apodsCount = 25
-        val dailyMills = 24L * 60 * 60 * 1000
-
-        // Get previous day before lastApodDate
-        val endDateUnix = lastApodDate - dailyMills
-        val endDateStr = endDateUnix.getApodStringDateFromUnix()
-
-        val difference = (apodsCount-1) * dailyMills
-
-        val statDateUnix = (lastApodDate - difference)
-        val startDateStr = statDateUnix.getApodStringDateFromUnix()
-
-        Timber.debug("fetch apods $startDateStr $endDateStr")
-
         val startDate = apodsDatesHelper.getStartDate(lastApodDate)
         val endDate = apodsDatesHelper.getEndDate(lastApodDate)
 
