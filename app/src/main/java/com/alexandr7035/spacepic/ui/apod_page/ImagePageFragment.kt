bce1bc906f55b4813df99868c2ed618e748fb734
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-04 16:31:40 +0300

    Add simple page for image apod

--- app/src/main/java/com/alexandr7035/spacepic/ui/apod_page/ImagePageFragment.kt
+++ app/src/main/java/com/alexandr7035/spacepic/ui/apod_page/ImagePageFragment.kt
@@ -1,39 +1,52 @@
 package com.alexandr7035.spacepic.ui.apod_page
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.alexandr7035.spacepic.R
 import com.alexandr7035.spacepic.core.extensions.debug
-import com.alexandr7035.spacepic.databinding.FragmentApodPageBinding
+import com.alexandr7035.spacepic.databinding.FragmentImagePageBinding
+import com.alexandr7035.spacepic.ui.ApodUi
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
-class ApodPageFragment : Fragment() {
+class ImagePageFragment : Fragment() {
 
-    private var binding: FragmentApodPageBinding? = null
+    private var binding: FragmentImagePageBinding? = null
     private val viewModel by viewModels<ApodPageViewModel>()
-    private val safeArgs by navArgs<ApodPageFragmentArgs>()
+    private val safeArgs by navArgs<ImagePageFragmentArgs>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        binding = FragmentApodPageBinding.inflate(inflater, container, false)
+        binding = FragmentImagePageBinding.inflate(inflater, container, false)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding?.toolbar?.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+
         val apodDate = safeArgs.apodDate
         viewModel.init(apodDate)
 
         viewModel.singleApodLiveData.observe(viewLifecycleOwner, {
             Timber.debug("apod: $it")
+            val apod = it as ApodUi.ImageApod
+
+            binding?.titleView?.text = apod.title
+            binding?.descriptionView?.text = apod.description
+            // FIXME
+            binding?.toolbar?.title = apod.date
+            Glide.with(binding!!.root.context).load(apod.imageUrl).into(binding!!.imageView)
         })
     }
 
--- app/src/main/java/com/alexandr7035/spacepic/ui/apods_list/ApodsListFragment.kt
+++ app/src/main/java/com/alexandr7035/spacepic/ui/apods_list/ApodsListFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alexandr7035.spacepic.core.extensions.debug
 import com.alexandr7035.spacepic.core.extensions.getApodStringDateFromUnix
 import com.alexandr7035.spacepic.core.extensions.getApodUnixDateFromString
+import com.alexandr7035.spacepic.core.extensions.navigateSafe
 import com.alexandr7035.spacepic.databinding.FragmentApodsListBinding
 import com.alexandr7035.spacepic.ui.ApodUi
 import dagger.hilt.android.AndroidEntryPoint
@@ -137,7 +139,8 @@ class ApodsListFragment : Fragment(), ImageClickListener, VideoClickListener {
     }
 
     override fun onImageApodClicked(apodDate: String) {
-        Timber.debug("clicked image $apodDate")
+        val date = apodDate.getApodUnixDateFromString()
+        findNavController().navigateSafe(ApodsListFragmentDirections.actionApodsListFragmentToImagePageFragment(date))
     }
 
     override fun onVideoApodClicked(apodDate: String) {
